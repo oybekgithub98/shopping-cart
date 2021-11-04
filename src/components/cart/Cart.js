45f65b6c16d95694928954a1f7ad53ac9b3a0743
oybekgithub98 <oybekgithub98@gmail.com>
2021-11-04 19:05:02 +0500

    added StripeCheckout

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Cart.css';
+import StripeCheckout from "react-stripe-checkout";
 
 const Cart = ({ cartItems, handleAddProduct, handleRemoveProduct }) => {
 
+
     const totalPrice = cartItems.reduce((price, item) => price + item.quantity * item.price, 0);
+
+
 
     return (
         <div className="cart">
             <div className="cart_header">
-                cart items 
+                cart items
                 {
                     cartItems.quantity
                 }
@@ -21,61 +25,48 @@ const Cart = ({ cartItems, handleAddProduct, handleRemoveProduct }) => {
             <div>
                 {cartItems.map((item) => {
                     return (
-                        <div key={item.id} className="cart_items_list">
-                            <img className="cart_items_image" src={item.image} alt={item.name} />
-                            <div className="cart_items_name">{item.name}</div>
-                            <div className="cart_items_function">
-                                <button className="cart_item_add" onClick={() => handleAddProduct(item)}>+</button>
-                                <button className="cart_item_remove" onClick={() => handleRemoveProduct(item)}>-</button>
+                        <>
+                            <div key={item.id} className="cart_items_list">
+                                <img className="cart_items_image" src={item.image} alt={item.name} />
+                                <div className="cart_items_name">{item.name}</div>
+                                <div className="cart_items_function">
+                                    <button className="cart_item_add" onClick={() => handleAddProduct(item)}>+</button>
+                                    <button className="cart_item_remove" onClick={() => handleRemoveProduct(item)}>-</button>
+                                </div>
+                                <div className="cart_items_price">
+                                    {item.quantity} * ${item.price}
+                                </div>
                             </div>
-                            <div className="cart_items_price">
-                                {item.quantity} * ${item.price}
-                            </div>
-                        </div>
+                        </>
                     )
                 })}
             </div>
             <div className="cart_items_total_price_name">
-                <p>Total price</p>
-                <div className="cart_items_total_price">${totalPrice}</div>
+                {
+                    ! cartItems.length ? <p>Total price</p> : <div className="cart_items_total_price">${totalPrice}</div>
+                }
+                
+
+                {
+                    !cartItems.length ?
+                        <div className="cart_items_total_price">${totalPrice}</div>
+                        :
+                        <div>
+                            <StripeCheckout
+                                stripeKey="pk_test_HnF0cQhq9UGw2GvWRltNiAQM00kn9HlRCg"
+                                billingAddress
+                                shippingAddress
+                                name="all products in the cart"
+                            />
+                        </div>
+
+                }
+
             </div>
         </div>
     )
 
+
 }
 
 export default Cart;
-
-
-// import React from 'react';
-// import './Cart.css';
-
-// const Cart = () => {
-//     return (
-//         <div className="cart">
-//             <div className="cart_header">
-//                 cart items
-//             </div>
-//             <div>
-//                 <div className="cart_items_list">
-//                     <img className="cart_items_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSox-_lyQyhGSn5GX-5BA5MI8gig9D0iAK2aQ&usqp=CAU" />
-//                     <div className="cart_items_name">Telefon</div>
-//                     <div className="cart_items_function">
-//                         <button className="cart_item_add" >+</button>
-//                         <button className="cart_item_remove">-</button>
-//                     </div>
-//                     <div className="cart_items_price">
-//                         1 * 1998
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="cart_items_total_price_name">
-//                 Total price
-//                 <div className="cart_items_total_price">99999</div>
-//             </div>
-//         </div>
-//     )
-
-// }
-
-// export default Cart;
